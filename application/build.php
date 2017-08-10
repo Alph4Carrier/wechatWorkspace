ac185d1bc884eded51489721e27a4325246ef3fc
Alan <4lphaCarrier@gmail.com>
2017-08-11 00:00:59 +0800

    Initial commit

---
+++ application/build.php
@@ -0,0 +1,34 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+return [
+    // 生成应用公共文件
+    //'__file__' => ['common.php', 'config.php', 'database.php'],
+
+    // 其他更多的模块定义
+    'config'    => [
+        '__file__'   => ['common.php', 'config.php', 'database.php'],
+    ],
+    'common'     => [
+        '__dir__'    => ['model','config'],
+        'model'      => ['Index','Login'],
+    ],
+    'home'     => [
+        '__dir__'    => ['controller', 'view'],
+        'controller' => ['Index'],
+        'view'       => ['index'],
+    ],
+    'login'     => [
+        '__dir__'    => ['controller', 'view'],
+        'controller' => ['Index'],
+        'view'       => ['login'],
+    ],
+];
